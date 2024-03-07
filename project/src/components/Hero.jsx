781d05cd5951b9d9f581294a56bf59063cc0e59b
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2024-03-07 01:34:41 -0500

    In Progress

--- project/src/components/Hero.jsx
+++ project/src/components/Hero.jsx
@@ -1,18 +1,19 @@
 import Section from "./section";
-import { curve } from "../assets";
+import { curve, heroBackground, robot } from "../assets";
 import Button from "./Button";
+
 const hero = () => {
   return (
     <Section
-      className="pt-[12rem -mt-[5.25]"
+      className="pt-[12rem] -mt-[5.25]"
       crosses
       crossesOffset="lg:translate-y-[5.25rem]"
       customPaddings
-      id="ero"
+      id="hero"
     >
-      <div className="container relative ">
-        <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb:[6rem]">
-          <h1 className="h1 mb-6 ">
+      <div className="container relative">
+        <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb-[6rem]">
+          <h1 className="h1 mb-6">
             Explore the possibilities of AI
             <span className="inline-block relative">
               Brainwave
@@ -22,15 +23,39 @@ const hero = () => {
                 width={624}
                 height={28}
                 alt="Curve"
-              ></img>{" "}
+              />
             </span>
           </h1>
           <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
-            Unleash the power{" "}
+            Unleash the power
           </p>
-          <Button href="/pricing " white>
+          <Button href="/pricing" white>
             Get Started
           </Button>
+        </div>
+        <div className="relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-24">
+          <div className="relative z-1 p-0.5 rounded-2xl bg-comic-gradient">
+            <div className="relative bg-n-8 rounded-[1rem]">
+              <div className="relative bg-n-0 rounded-t-[0.9rem]" />
+              <div className="aspect-[33/40] rounded-b-[0.9rem] overflow-hidden md:aspect-[688/490] lg:aspect-[1024/490]" />
+              <img
+                src={robot}
+                className="w-full scale-1.7 translate-y-[8%] md:scale-[1] md:-translate-y-[10%] lg:translate-y-[23%] "
+                width={1024}
+                height={490}
+                alt="hero"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="absolute -top -[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%] ">
+          <img
+            src={heroBackground}
+            className="w-full"
+            width={1440}
+            height={1800}
+            alt="background"
+          ></img>
         </div>
       </div>
     </Section>
