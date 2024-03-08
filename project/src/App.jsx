999b7bf1b42148b17010e3ccc5d9fd53a92af345
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2024-03-08 15:50:32 -0500

    check

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,12 +1,12 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Footer from "./components/Footer";
 import Benefits from "./components/Benefits";
 import Collaboration from "./components/Collaboration";
 import Services from "./components/Services";
 import Pricing from "./components/Pricing";
 import Roadmap from "./components/Roadmap";
-import Footer from "./components/Footer";
 
 const App = () => {
   return (
