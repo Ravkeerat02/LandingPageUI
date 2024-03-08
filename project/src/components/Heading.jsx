060b5aab3781fc675a2ef6cf7f6385c11d6c43b8
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2024-03-08 15:30:38 -0500

    Pricing section complete

--- project/src/components/Heading.jsx
+++ project/src/components/Heading.jsx
@@ -1,7 +1,11 @@
-const Heading = ({ className, title }) => {
+import TagLine from "./TagLine";
+
+const Heading = ({ className, title, text, tag }) => {
   return (
     <div className={`${className} max-2-[50rem] mx-auto mb-12 lg:mb-20 `}>
+      {tag && <TagLine className="mb-4 md:justify-center "></TagLine>}
       {title && <h2 className="h2">{title}</h2>}
+      {text && <p className="body-2 mt-4 text-n-4 ">{text}</p>}
     </div>
   );
 };
